3356b53b5c7295debfa2be2a744afb9e89d0cf5b
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-02-18 18:00:24 -0300

    chore: iniciando projeto a partir do spring initializr com dependencias spring web, postgresql driver, jpa.

---
+++ src/test/java/com/example/escolaDB/EscolaDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.escolaDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
